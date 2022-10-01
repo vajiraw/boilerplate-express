91814bd3d30e656b850a1a7a88f7a1c578ba713e
Vajira <vajirawke@gmail.com>
2022-10-01 15:54:17 +0530

    logger chhanged to root

--- myApp.js
+++ myApp.js
@@ -5,10 +5,7 @@ require('dotenv').config()
 //app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
-app.use((req, res, next) => {
-  console.log(req.method+" "+req.ip);  
-  next();
-})
+
 
 
 
