c18dad1ee67c511aae3727c170ca8cf55ae1919e
Vajira <vajirawke@gmail.com>
2022-10-01 12:13:15 +0530

    server static assets

--- myApp.js
+++ myApp.js
@@ -9,7 +9,6 @@ app.get('/',(req, res) =>{
 
 
 
-
   
 
 
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var myApp = require('./myApp');
 var express = require('express');
 var app = express();
 
+// using middleware muoount public folder
+app.use("/public", express.static(__dirname + "/public"));
+
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
